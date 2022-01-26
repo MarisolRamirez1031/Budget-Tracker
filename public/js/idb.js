3c0236a8dfcafb0fb0b7fe2a31c79bc59e746079
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-25 23:47:29 -0600

    added some properties to manifest.json

--- public/js/idb.js
+++ public/js/idb.js
@@ -35,21 +35,22 @@ function saveRecord(record) {
     const transaction = db.transaction(['new_transaction'], 'readwrite');
   
     // access the object store for `new_transaction`
-    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
   
     // add record to your store with add method
-    transactionObjectStore.add(record);
+    budgetObjectStore.add(record);
 }
+
 
 function uploadTransaction() {
     // open a transaction on your db
     const transaction = db.transaction(['new_transaction'], 'readwrite');
   
     // access your object store
-    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
   
     // get all records from store and set to a variable
-    const getAll = transactionObjectStore.getAll();
+    const getAll = budgetObjectStore.getAll();
   
 }
 
@@ -73,9 +74,9 @@ getAll.onsuccess = function() {
           // open one more transaction
           const transaction = db.transaction(['new_transaction'], 'readwrite');
           // access the new_transaction object store
-          const transactionObjectStore = transaction.objectStore('new_transaction');
+          const budgetObjectStore = transaction.objectStore('new_transaction');
           // clear all items in your store
-          transactionObjectStore.clear();
+          budgetObjectStore.clear();
 
           alert('All saved transactions has been submitted!');
         })
