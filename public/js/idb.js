b831a5bace87462503044400506b9602bc3c327d
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-25 19:39:51 -0600

    save data to IndexedDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,8 +9,8 @@ request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
 
-    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_budget', { autoIncrement: true });
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
   };
 
 // upon a successful 
@@ -28,3 +28,15 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+
+// This function will be executed if we attempt to submit info and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_transaction`
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    transactionObjectStore.add(record);
+  }
